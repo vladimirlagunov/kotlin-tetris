bf220655b818d73dc189cefada1b68776aeae950
Vladimir Lagunov <lagunov@yandex-team.ru>
2018-12-14 00:39:56 +0700

    ignore/remove excess files

--- make_js_html.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo '<!DOCTYPE html>'
-echo '<html><body><script type="text/javascript">'
-
-npx terser build/classes/kotlin/js/main/lib/kotlin.js
-npx terser build/classes/kotlin/js/main/kotlin-tetris.js
-
-echo '</script></body></html>'
